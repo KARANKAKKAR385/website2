1f28ff01fcc39b34f6cb324fb3f62520e16799ab
KARANKAKKAR385 <143103376+KARANKAKKAR385@users.noreply.github.com>
2024-05-21 14:00:08 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+// Change background color on button click
+document.getElementById("changeColorBtn").addEventListener("click", function() {
+    document.body.style.backgroundColor = getRandomColor();
+  });
 
+  // Function to generate a random color
+  function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+
+  // Smooth scrolling to anchor links
+  document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+              behavior: 'smooth'
+          });
+      });
+  });
