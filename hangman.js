360d92ce46f5270a038a4388710a8f1f55c9c3a5
KARANKAKKAR385 <143103376+KARANKAKKAR385@users.noreply.github.com>
2024-05-21 13:55:55 -0700

    Create hangman.js

---
+++ hangman.js
@@ -0,0 +1,125 @@
+const wordEl = document.getElementById('word')
+const wrongLettersEl = document.getElementById('wrong-letters')
+const playAgainBtn = document.getElementById('play-button')
+const popup = document.getElementById('popup-container')
+const notification = document.getElementById('notification-container')
+const finalMessage = document.getElementById('final-message')
+const figureParts = document.querySelectorAll('.figure-part')
+
+const word = ['hogrider', 'larry', 'megaknight', 'lebron']
+
+let selectedIndex = Math.floor(word.length * Math.random())
+let selectedWord = word[selectedIndex]
+
+const correctLetters = []
+const wrongLetters = []
+
+//show hidden word
+function displayWord() {
+    wordEl.innerHTML = `
+        ${selectedWord
+            .split('')
+            .map(letter => `
+                <span class="letter">
+                    ${correctLetters.includes(letter) ? letter : ''}
+                </span>
+            `).join('')
+        }
+
+    `
+    const innerWord = wordEl.innerText.replace(/\n/g, '')
+
+    if (innerWord == selectedWord) {
+        finalMessage.innerText = 'Congratulations! You Won!'
+        popup.style.display = 'flex'
+
+        window.addEventListener('keydown', e => {
+
+            if (e.keyCode >= 65 && e.keyCode <= 90) {
+                pause()
+            }
+        })
+
+    }
+}
+
+//Keydown letter press
+window.addEventListener('keydown', e => {
+
+    if (e.keyCode >= 65 && e.keyCode <= 90) {
+        const letter = e.key
+
+        if (selectedWord.includes(letter)) {
+            if (!correctLetters.includes(letter)) {
+                correctLetters.push(letter)
+
+                displayWord()
+            }else {
+                showNotification()
+            }} else {
+                if (!wrongLetters.includes(letter)) {
+                    wrongLetters.push(letter)
+
+                    updateWrongLettersEl()
+                }else {
+                    showNotification()
+                }
+            }
+        }
+})
+
+//update wotn gletters
+function updateWrongLettersEl() {
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `
+
+//dislpay parteys
+ figureParts.forEach((part, index) => {
+    const errors = wrongLetters.length
+
+    //check if lost
+ if (index < errors) {
+    part.style.display = 'block'
+ } else{
+    part.style.display = 'none'
+ }
+ })
+
+
+
+
+ //cehck loss
+ if (wrongLetters.length == figureParts.length) {
+    finalMessage.innerText = 'Unfortunately you lost!'
+    popup.style.display = 'flex'
+ }
+}
+
+//show noti
+function showNotification() {
+    notification.classList.add('show')
+
+    setTimeout(() => {
+        notification.classList.remove('show')
+    }, 2000)
+}
+
+//restart gamed
+playAgainBtn.addEventListener('click', () => {
+    correctLetters.length = 0
+    wrongLetters.length = 0
+
+    selectedIndex = Math.floor(word.length * Math.random())
+    selectedWord = word[selectedIndex]
+
+    displayWord()
+
+    updateWrongLettersEl()
+
+    popup.style.display = 'none'
+})
+
+displayWord()
+
